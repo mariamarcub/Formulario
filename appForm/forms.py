a630db68918aad549b2cbbdab525da720cc50869
Mariamarcub <mmarcub912@iesmartinezm.es>
2023-11-08 23:53:21 +0100

    nuevamente terminado

--- appForm/forms.py
+++ appForm/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+import re
+
 
 DIAS_SEMANA = [
     ('Lunes', 'Lunes'),
@@ -12,27 +14,26 @@ DIAS_SEMANA = [
 
 
 class CreaFormularioForm(forms.Form):
-    fechaInicio = forms.DateField(label='Fecha inicial', required=True, input_formats=['%d/%m/%Y'],
-                                  help_text="Formato de la fecha: dd/MM/yyyy")
-    fechaFin = forms.DateField(label='Fecha final', required=True, input_formats=['%d/%m/%Y'],
-                               help_text="Formato de la fecha: dd/MM/yyyy")
-    diasSemana = forms.MultipleChoiceField(label='Dias de la semana', required=True,
-                                           widget=forms.CheckboxSelectMultiple, choices=DIAS_SEMANA)
+    fechaInicio = forms.DateField(label='Fecha inicial', required=True, input_formats=['%d/%m/%Y'],help_text="Formato de la fecha: dd/MM/yyyy")
+    fechaFin = forms.DateField(label='Fecha final', required=True, input_formats=['%d/%m/%Y'],help_text="Formato de la fecha: dd/MM/yyyy")
+    diasSemana = forms.MultipleChoiceField(label='Dias de la semana', required=True,widget=forms.CheckboxSelectMultiple, choices=DIAS_SEMANA)
     email = forms.EmailField(label='Email', required=True, help_text='Formato del email: xxx@iesmartinezm.es')
 
     def clean(self):
         cleaned_data = super().clean()
-        fechaInicio = self.cleaned_data['fechaInicio']
-        fechaFin = self.cleaned_data['fechaFin']
-        diasSemana = self.cleaned_data['diasSemana']
-        email = self.cleaned_data['email']
+        fechaInicio = cleaned_data.get('fechaInicio')
+        fechaFin = cleaned_data.get('fechaFin')
+        diasSemana = cleaned_data.get('diasSemana')
+        email = cleaned_data.get('email')
 
-        if fechaFin < fechaInicio:  # si es 3 de noviembre el inicio, la final tiene q ser 4 o más
-            raise forms.ValidationError(
-                "Error: No puede ser la fecha final anterior a la inicial. Debe ser igual o superior a la fecha inicial.")
+        if fechaInicio and fechaFin: #Sirve para verificar que tienen valores y no son NONE
+                                     #con AND vemos que ambas son verdaderas
+            if fechaFin < fechaInicio:
+                raise forms.ValidationError("Error: No puede ser la fecha final anterior a la inicial. Debe ser igual o superior a la fecha inicial.")
+        if diasSemana:
+            if len(diasSemana)<1 or len(diasSemana)>3:
+                raise forms.ValidationError("Error: No puedes seleccionar menos de 1 días y más de 3 días")
 
-        if len(diasSemana) >= 1 or len(diasSemana) > 3:
-            raise forms.ValidationError('No se puede señalar más de tres días de la semana.')
+        if email and not email.endswith('@iesmartinezm.es'): #Si es el campo email no es NONE y además, no termina en @iesmartinezm.es
+            raise forms.ValidationError("El email debe ser de dominio @iesmartinezm.es")
 
-        if not email.endswith('@iesmartinezm.es'):
-            raise forms.ValidationError('El formato del email debe ser xxx@martinezm.es')
